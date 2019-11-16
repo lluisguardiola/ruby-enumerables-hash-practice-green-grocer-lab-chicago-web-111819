1d94409ab5c5b9454249a383725d8cd3a5fb7076
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-11-16 19:49:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@ def consolidate_cart(cart)
     hash.each do |item, attributes|
       new_cart[item] = attributes if !new_cart[item]
       
-      if new_cart[item][:count]
+      if !new_cart[item][:count]
         new_cart[item][:count] = 1
       else
         new_cart[item][:count] += 1
