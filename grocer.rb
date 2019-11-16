3b1f2cf25349d27f750aca9e5b6ff178f2a76a36
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-11-16 20:40:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,8 +2,8 @@ def consolidate_cart(cart)
   new_cart = {}
   
   cart.each do |hash|
-    hash.each do |item, attribute|
-      new_cart[item] = attribute if !new_cart[item]
+    hash.each do |item, attributes|
+      new_cart[item] = attributes if !new_cart[item]
       
       if !new_cart[item][:count]
         new_cart[item][:count] = 1
@@ -35,7 +35,8 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  cart.each do |item, attribute|
+  cart.each do |item, attributes|
+    
 end
 
 def checkout(cart, coupons)
